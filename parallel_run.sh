4145a293f9ced20f7c68215dbd245068012ffeaf
hridaik <hridaikhurana@gmail.com>
2026-01-12 11:06:54 +0100

    new mining

--- parallel_run.sh
+++ parallel_run.sh
@@ -1,8 +1,9 @@
+echo "Initializing database.."
+python -c "import optuna; optuna.create_study(storage='sqlite:///calibration.db', study_name='calibration', load_if_exists=True)"
+echo "Database initialized"
 for i in {1..8}; do
     echo "Launching Worker $i..."
-    # "nice -n 10" prevents SSH from lagging
-    # "2>&1" captures CRASHES into the log file
-    nice -n 10 python -u optuna_fit.py > worker_${i}.log 2>&1 &
+    nice -n 10 python -u calibrate.py > worker_${i}.log 2>&1 &
 done
 # keep screen alive
 wait
